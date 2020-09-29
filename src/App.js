a75a9bd08fb4f65bf3e23eb38699f92c9cd1fcf4
AnbolKing <272344341@qq.com>
2020-09-29 16:17:53 +0800

    :sparkles: 完成一个slide-loading页面预加载动画

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component} from 'react';
-import './App.css';
-import Header from './components/Header';
-import Test from './components/Test';
+// import './App.css';
+// import Header from './components/Header';
+// import Test from './components/Test';
+import SlideLoad from './components/Lazy/slideLoad/index';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <Header></Header>
-        <Test></Test>
+        {/* <Header></Header>
+        <Test></Test> */}
+        <SlideLoad></SlideLoad>
       </div>
     )
   }
---
+++ src/components/Lazy/slideLoad/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const SlideLoad = () => {
+  return (
+    <div className="slide-container">
+      <div className="slide-loader">
+        Loading. . . 
+      </div>
+    </div>
+  )
+}
+
+export default SlideLoad;
